525506ec516e2d087171d9a3cb20c186ece87f45
Priyanshu Pansari <Priyanshu.Pansari@gmail.com>
2025-02-27 01:30:52 +0530

    added vm-config

--- flake.nix
+++ flake.nix
@@ -3,28 +3,41 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
     jovian-nixos.url = "github:Jovian-Experiments/Jovian-NixOS";
-};
-  outputs = { self, nixpkgs, sops-nix, jovian-nixos, ... }@inputs: {
-    # replace 'joes-desktop' with your hostname here.
-    nixosConfigurations ={
-
-    PandorasBox = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-       sops-nix.nixosModules.sops
-      ./hosts/PandorasBox/default.nix
-      ./configuration.nix ];
-    };
-
-    Hope = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-       sops-nix.nixosModules.sops
-      ./hosts/Hope/default.nix
-      ./configuration.nix ];
-    };
   };
+
+  outputs = { self, nixpkgs, sops-nix, jovian-nixos, ... }@inputs: {
+    nixosConfigurations = {
+      # Your existing configurations
+      PandorasBox = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          sops-nix.nixosModules.sops
+          ./hosts/PandorasBox/default.nix
+          ./configuration.nix
+        ];
+      };
+      
+      Hope = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          sops-nix.nixosModules.sops
+          ./hosts/Hope/default.nix
+          ./configuration.nix
+        ];
+      };
+      
+      # VM configuration
+      VM = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          sops-nix.nixosModules.sops
+          jovian-nixos.nixosModules.default
+          ./vm-configuration.nix  # Save the VM config to this file
+        ];
+      };
+    };
   };
 }
