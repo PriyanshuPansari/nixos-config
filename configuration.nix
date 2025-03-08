10d38434ca5f74f5543b015b3dd2a27d7df0de47
PriyanshuPansari <Priyanshu.Pansari@gmail.com>
2025-03-08 17:39:44 +0530

    added nvd keybinds

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,7 @@
           };
           statusline.lualine.enable = true;
           telescope.enable = true;
+          navigation.harpoon.enable = true;
           autocomplete.nvim-cmp.enable = true;
           utility.yazi-nvim.enable = true;
           extraLuaFiles = [
@@ -84,12 +85,327 @@
               name = "init-lua";
             })
           ];
+          # General keymaps
           keymaps = [
+            # Save and quit
             {
-              key = "<leader>f";
+              key = "<leader>wq";
               mode = "n";
-              silent = true;
-              action = ":vim.lsp.buf.format";
+              action = ":wq<CR>";
+            }
+            # Quit without saving
+            {
+              key = "<leader>qq";
+              mode = "n";
+              action = ":q!<CR>";
+            }
+            # Save
+            {
+              key = "<leader>ww";
+              mode = "n";
+              action = ":w<CR>";
+            }
+            # Open URL under cursor
+            {
+              key = "gx";
+              mode = "n";
+              action = ":!open <c-r><c-a><CR>";
+            }
+            # Quit all
+            {
+              key = "<leader>qa";
+              mode = "n";
+              action = ":qa<CR>";
+            }
+            # Save all
+            {
+              key = "<leader>wa";
+              mode = "n";
+              action = ":wa<CR>";
+            }
+
+            # Navigate vim panes
+            {
+              key = "<c-k>";
+              mode = "n";
+              action = ":wincmd k<CR>";
+            }
+            {
+              key = "<c-j>";
+              mode = "n";
+              action = ":wincmd j<CR>";
+            }
+            {
+              key = "<c-h>";
+              mode = "n";
+              action = ":wincmd h<CR>";
+            }
+            {
+              key = "<c-l>";
+              mode = "n";
+              action = ":wincmd l<CR>";
+            }
+
+            # Split window management
+            {
+              key = "<leader>sv";
+              mode = "n";
+              action = "<C-w>v";
+            }
+            {
+              key = "<leader>sg";
+              mode = "n";
+              action = "<C-w>s";
+            }
+            {
+              key = "<leader>se";
+              mode = "n";
+              action = "<C-w>=";
+            }
+            {
+              key = "<leader>sx";
+              mode = "n";
+              action = ":close<CR>";
+            }
+            {
+              key = "<leader>sj";
+              mode = "n";
+              action = "<C-w>-";
+            }
+            {
+              key = "<leader>sk";
+              mode = "n";
+              action = "<C-w>+";
+            }
+            {
+              key = "<leader>sl";
+              mode = "n";
+              action = "<C-w>>5";
+            }
+            {
+              key = "<leader>sh";
+              mode = "n";
+              action = "<C-w><5";
+            }
+
+            # Tab management
+            {
+              key = "<leader>to";
+              mode = "n";
+              action = ":tabnew<CR>";
+            }
+            {
+              key = "<leader>tx";
+              mode = "n";
+              action = ":tabclose<CR>";
+            }
+            {
+              key = "<leader>tn";
+              mode = "n";
+              action = ":tabn<CR>";
+            }
+            {
+              key = "<leader>tp";
+              mode = "n";
+              action = ":tabp<CR>";
+            }
+            {
+              key = "<leader>1";
+              mode = "n";
+              action = ":tabn 1<CR>";
+            }
+            {
+              key = "<leader>2";
+              mode = "n";
+              action = ":tabn 2<CR>";
+            }
+            {
+              key = "<leader>3";
+              mode = "n";
+              action = ":tabn 3<CR>";
+            }
+            {
+              key = "<leader>4";
+              mode = "n";
+              action = ":tabn 4<CR>";
+            }
+            {
+              key = "<leader>5";
+              mode = "n";
+              action = ":tabn 5<CR>";
+            }
+            {
+              key = "<leader>6";
+              mode = "n";
+              action = ":tabn 6<CR>";
+            }
+            {
+              key = "<leader>7";
+              mode = "n";
+              action = ":tabn 7<CR>";
+            }
+            {
+              key = "<leader>8";
+              mode = "n";
+              action = ":tabn 8<CR>";
+            }
+            {
+              key = "<leader>9";
+              mode = "n";
+              action = ":tabn 9<CR>";
+            }
+
+            # Diff keymaps
+            {
+              key = "<leader>cc";
+              mode = "n";
+              action = ":diffput<CR>";
+            }
+            {
+              key = "<leader>cj";
+              mode = "n";
+              action = ":diffget 1<CR>";
+            }
+            {
+              key = "<leader>ck";
+              mode = "n";
+              action = ":diffget 3<CR>";
+            }
+            {
+              key = "<leader>cn";
+              mode = "n";
+              action = "]c";
+            }
+            {
+              key = "<leader>cp";
+              mode = "n";
+              action = "[c";
+            }
+
+            # Quickfix keymaps
+            {
+              key = "<leader>qo";
+              mode = "n";
+              action = ":copen<CR>";
+            }
+            {
+              key = "<leader>qf";
+              mode = "n";
+              action = ":cfirst<CR>";
+            }
+            {
+              key = "<leader>qn";
+              mode = "n";
+              action = ":cnext<CR>";
+            }
+            {
+              key = "<leader>qp";
+              mode = "n";
+              action = ":cprev<CR>";
+            }
+            {
+              key = "<leader>ql";
+              mode = "n";
+              action = ":clast<CR>";
+            }
+            {
+              key = "<leader>qc";
+              mode = "n";
+              action = ":cclose<CR>";
+            }
+
+            # Telescope
+            {
+              key = "<leader>ff";
+              mode = "n";
+              action = ''lua require('telescope.builtin').find_files()'';
+            }
+            {
+              key = "<leader>fg";
+              mode = "n";
+              action = ''lua require('telescope.builtin').live_grep()'';
+            }
+            {
+              key = "<leader>fb";
+              mode = "n";
+              action = ''lua require('telescope.builtin').buffers()'';
+            }
+            {
+              key = "<leader>fh";
+              mode = "n";
+              action = ''lua require('telescope.builtin').help_tags()'';
+            }
+            {
+              key = "<leader>fs";
+              mode = "n";
+              action = ''lua require('telescope.builtin').current_buffer_fuzzy_find()'';
+            }
+            {
+              key = "<leader>fo";
+              mode = "n";
+              action = ''lua require('telescope.builtin').lsp_document_symbols()'';
+            }
+            {
+              key = "<leader>fi";
+              mode = "n";
+              action = ''lua require('telescope.builtin').lsp_incoming_calls()'';
+            }
+
+            # Harpoon
+            {
+              key = "<leader>ha";
+              mode = "n";
+              action = ''lua require("harpoon.mark").add_file()'';
+            }
+            {
+              key = "<leader>hh";
+              mode = "n";
+              action = ''lua require("harpoon.ui").toggle_quick_menu()'';
+            }
+            {
+              key = "<leader>h1";
+              mode = "n";
+              action = ''lua require("harpoon.ui").nav_file(1)'';
+            }
+            {
+              key = "<leader>h2";
+              mode = "n";
+              action = ''lua require("harpoon.ui").nav_file(2)'';
+            }
+            {
+              key = "<leader>h3";
+              mode = "n";
+              action = ''lua require("harpoon.ui").nav_file(3)'';
+            }
+            {
+              key = "<leader>h4";
+              mode = "n";
+              action = ''lua require("harpoon.ui").nav_file(4)'';
+            }
+            {
+              key = "<leader>h5";
+              mode = "n";
+              action = ''lua require("harpoon.ui").nav_file(5)'';
+            }
+            {
+              key = "<leader>h6";
+              mode = "n";
+              action = ''lua require("harpoon.ui").nav_file(6)'';
+            }
+            {
+              key = "<leader>h7";
+              mode = "n";
+              action = ''lua require("harpoon.ui").nav_file(7)'';
+            }
+            {
+              key = "<leader>h8";
+              mode = "n";
+              action = ''lua require("harpoon.ui").nav_file(8)'';
+            }
+            {
+              key = "<leader>h9";
+              mode = "n";
+              action = ''lua require("harpoon.ui").nav_file(9)'';
             }
           ];
           languages = {
