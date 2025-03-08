4e904582cd6b2291d26a7e113082fa2005041f77
PriyanshuPansari <Priyanshu.Pansari@gmail.com>
2025-03-08 17:53:02 +0530

    correcte harpoon keybinds

--- configuration.nix
+++ configuration.nix
@@ -43,11 +43,9 @@
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
-
-
-      nvidia-container-toolkit.enable = true;
-
     };
+    nvidia-container-toolkit.enable = true;
+
   };
 
   programs = {
